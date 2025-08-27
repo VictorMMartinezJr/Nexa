ff8d12c6e5e99f4ea1d04e1f71ffb66d8b6e0ef6
Victor <victorm.martinezjr@outlook.com>
2025-08-26 22:28:48 -0400

    initialize backend

---
+++ backend/nexa/src/main/java/in/victormartinezjr/nexa/NexaApplication.java
@@ -0,0 +1,13 @@
+package in.victormartinezjr.nexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexaApplication.class, args);
+    }
+
+}
