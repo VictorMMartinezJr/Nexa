64716424a09397c71eeca385e5a5db2ae4448300
Victor <victorm.martinezjr@outlook.com>
2025-09-08 22:21:38 -0400

    add dearchInput state to SearchbarContainer

--- frontend/nexa/src/components/Navbar/SearchbarContainer.jsx
+++ frontend/nexa/src/components/Navbar/SearchbarContainer.jsx
@@ -1,13 +1,19 @@
 import { FiSearch } from "react-icons/fi";
+import logo from "../../assets/logo.svg";
+import { useState } from "react";
 
 const SearchbarContainer = ({ state, setState }) => {
+  const [searchInput, setSearchInput] = useState("");
+
   return (
     <div
       className={`flex justify-between items-center bg-white h-[8vh] w-full px-4 absolute top-0 right-0 transform transition-transform duration-300 ease-in-out ${
         state ? "translate-x-0" : "translate-x-full"
       } 2xl:px-8`}
     >
-      <p className="hidden lg:block lg:w-1/4">Logo</p>
+      <div className="hidden lg:block lg:w-1/4">
+        <img src={logo} className="lg:block lg:w-12 lg:h-12" />
+      </div>
 
       {/* Search form */}
       <form className="flex items-center justify-between w-full relative text-lg gap-4">
@@ -22,7 +28,9 @@ const SearchbarContainer = ({ state, setState }) => {
         <input
           type="text"
           placeholder="Search"
-          className="bg-gray-200 rounded-2xl px-8 py-1 lg:w-3/4"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          className="bg-gray-200 rounded-2xl px-8 py-1 w-[90%] lg:w-3/4"
         />
         {/* Cancel button */}
         <button
