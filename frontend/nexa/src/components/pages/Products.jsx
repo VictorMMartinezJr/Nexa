2ac11b80e1e6ae5c1e38595f039ed7f543cbb0d9
Victor <victorm.martinezjr@outlook.com>
2025-09-12 20:40:44 -0400

    add filters component to products page

--- frontend/nexa/src/components/pages/Products.jsx
+++ frontend/nexa/src/components/pages/Products.jsx
@@ -1,29 +1,247 @@
+import { useState } from "react";
 import ProductGrid from "../ProductGrid";
 import FilterSlider from "../Slider/FilterSlider";
 import { IoOptionsOutline } from "react-icons/io5";
+import { RiCloseFill } from "react-icons/ri";
 
 const Products = () => {
+  const [showFilters, setShowFilters] = useState(false);
+  const [selectedSizes, setSelectedSizes] = useState([]);
+  const [sortOption, setSortOption] = useState("");
+
+  const handleSortChange = (e) => {
+    setSortOption(e.target.value);
+  };
+
+  const handleSizeChange = (e) => {
+    const { value, checked } = e.target;
+
+    setSelectedSizes((prev) =>
+      checked ? [...prev, value] : prev.filter((size) => size !== value)
+    );
+  };
+
+  const handleSubmit = () => {
+    setShowFilters(false);
+    // Here you would typically also apply the filters to your product list
+  };
+
+  const resetFilters = () => {
+    setSelectedSizes([]);
+    setSortOption("");
+  };
+
   return (
     <div className="lg:px-4 2xl:px-8">
-      <h2 className="pt-8 px-4 lg:px-0">Mens Clothing</h2>
+      <h2 className="pt-8 px-4 lg:px-0 lg:hidden">Mens Clothing</h2>
+
       {/* Category slider */}
       <FilterSlider />
+
       {/* Result # and filter button */}
       <div className="w-full flex justify-between items-center py-4 px-4 lg:px-0">
-        <p>100 Results</p>
-        <button className="flex justify-center items-center gap-2 px-6 py-1.5 border-1 rounded-full cursor-pointer lg:px-6">
-          Filter
-          <IoOptionsOutline className="text-xl" />
-        </button>
+        <p className="lg:hidden">100 Results</p>
+        <p className="hidden lg:block lg:text-2xl">Men's Clothing (100)</p>
+
+        {/* Buttons */}
+        <div className="flex justify-center items-center gap-4">
+          {/* Filter button */}
+          <button
+            className="flex justify-center items-center gap-2 px-6 py-1.5 border-1 rounded-full cursor-pointer lg:px-6 lg:border-0"
+            onClick={() => setShowFilters(!showFilters)}
+          >
+            <p className="hidden lg:block">{showFilters ? "Hide " : "Show "}</p>
+            Filters
+            <IoOptionsOutline className="text-xl" />
+          </button>
+        </div>
       </div>
-      {/* Products grid */}
-      <div className="grid grid-cols-2 gap-2 lg:grid-cols-3">
-        <ProductGrid />
-        <ProductGrid />
-        <ProductGrid />
-        <ProductGrid />
-        <ProductGrid />
-        <ProductGrid />
+
+      {/* Mobile Filters */}
+      <div
+        className={`fixed flex flex-col gap-8 w-full h-screen bg-white top-0 left-0 z-10 p-4 text-xl transform transition-transform duration-500 ease-in-out ${
+          showFilters ? "translate-y-0" : "translate-y-full"
+        } lg:hidden`}
+      >
+        <div className="flex justify-between">
+          <p>Filters</p>
+          <button
+            onClick={() => {
+              resetFilters();
+              setShowFilters(false);
+            }}
+          >
+            <RiCloseFill className="self-end text-3xl cursor-pointer" />
+          </button>
+        </div>
+
+        {/* Sort Filters */}
+        <fieldset className="flex flex-col gap-4">
+          <legend className="py-4">Sort By</legend>
+
+          <label>
+            <input
+              type="radio"
+              name="sortMobile"
+              value="lowToHigh"
+              checked={sortOption === "lowToHigh"}
+              onChange={handleSortChange}
+              className="accent-black"
+            />
+            <span className="ml-2">Price: Low-High</span>
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="sortMobile"
+              value="highToLow"
+              checked={sortOption === "highToLow"}
+              onChange={handleSortChange}
+              className="accent-black"
+            />
+            <span className="ml-2">Price: High-Low</span>
+          </label>
+        </fieldset>
+
+        {/* Size Filters */}
+        <fieldset className="flex flex-col gap-4">
+          <legend className="py-4">Size</legend>
+
+          {/* Map sizes */}
+          <div className="flex justify-center gap-4">
+            {["S", "M", "L", "XL", "XXL"].map((size) => (
+              <label key={size}>
+                <input
+                  type="checkbox"
+                  value={size}
+                  checked={selectedSizes.includes(size)}
+                  onChange={handleSizeChange}
+                  className="peer hidden"
+                />
+                <span className="px-4 py-1 rounded-md border-1 border-gray-200 peer-checked:border-black">
+                  {size}
+                </span>
+              </label>
+            ))}
+          </div>
+        </fieldset>
+
+        {/* Clear & apply buttpns */}
+        <div className="flex justify-center gap-2 mt-4 text-md">
+          <button
+            className="border-2 border-gray-200 px-6 py-2 rounded-full cursor-pointer"
+            onClick={resetFilters}
+          >
+            Clear Filters
+          </button>
+          <button
+            className="bg-black text-white px-6 py-2 rounded-full cursor-pointer"
+            onClick={handleSubmit}
+          >
+            Apply Filters
+          </button>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        {/* Desktop Filters */}
+        {showFilters && (
+          <div
+            className={`hidden lg:flex flex-col gap-8 w-[30vw] h-screen p-4 text-xl transform transition-transform duration-500 ease-in-out ${
+              showFilters ? "-translate-x-0" : "-translate-x-full"
+            } 2xl:w-[20vw]`}
+          >
+            <div className="flex justify-between">
+              <p>Filters</p>
+              <button onClick={() => setShowFilters(false)}>
+                <RiCloseFill className="self-end text-3xl cursor-pointer" />
+              </button>
+            </div>
+
+            {/* Sort Filters */}
+            <fieldset className="flex flex-col gap-4">
+              <legend className="py-4">Sort By</legend>
+
+              <label>
+                <input
+                  type="radio"
+                  name="sortDesktop"
+                  value="lowToHigh"
+                  checked={sortOption === "lowToHigh"}
+                  className="accent-black"
+                  onChange={handleSortChange}
+                />
+                <span className="ml-2">Price: Low-High</span>
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  name="sortDesktop"
+                  value="highToLow"
+                  checked={sortOption === "highToLow"}
+                  className="accent-black"
+                  onChange={handleSortChange}
+                />
+                <span className="ml-2">Price: High-Low</span>
+              </label>
+            </fieldset>
+
+            {/* Size Filters */}
+            <fieldset className="flex flex-col gap-4">
+              <legend className="py-4">Size</legend>
+
+              {/* Map sizes */}
+              <div className="flex justify-center gap-4 flex-wrap">
+                {["S", "M", "L", "XL", "XXL"].map((size) => (
+                  <label key={size}>
+                    <input
+                      type="checkbox"
+                      value={size}
+                      checked={selectedSizes.includes(size)}
+                      onChange={handleSizeChange}
+                      className="peer hidden"
+                    />
+                    <span className="px-4 py-1 rounded-md border-1 border-gray-200 peer-checked:border-black">
+                      {size}
+                    </span>
+                  </label>
+                ))}
+              </div>
+            </fieldset>
+
+            {/* Clear & apply buttpns */}
+            <div className="flex justify-center gap-2 mt-4 text-md">
+              <button
+                className="border-2 border-gray-200 px-3 py-2 rounded-full cursor-pointer"
+                onClick={resetFilters}
+              >
+                Clear Filters
+              </button>
+              <button
+                className="bg-black text-white px-3 py-2 rounded-full cursor-pointer"
+                onClick={handleSubmit}
+              >
+                Apply Filters
+              </button>
+            </div>
+          </div>
+        )}
+
+        {/* Products grid */}
+        <div className="flex-1 grid grid-cols-2 gap-2 lg:grid-cols-3">
+          <ProductGrid />
+          <ProductGrid />
+          <ProductGrid />
+          <ProductGrid />
+          <ProductGrid />
+          <ProductGrid />
+          <ProductGrid />
+          <ProductGrid />
+          <ProductGrid />
+          <ProductGrid />
+        </div>
       </div>
     </div>
   );
