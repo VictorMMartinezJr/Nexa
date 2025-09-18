b0455d31701d15cb4d6f27000298e90eff8d4b1a
Victor <victorm.martinezjr@outlook.com>
2025-09-18 16:45:32 -0400

    create context api

---
+++ frontend/nexa/src/context/AppContext.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { createContext, useState } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+  const [products, setProducts] = useState([]);
+  const [sortOption, setSortOption] = useState(null);
+  const [audience, setAudience] = useState(null);
+  const [category, setCategory] = useState(null);
+
+  const fetchProducts = async (url, sortOption, audience, category) => {
+    try {
+      const response = await axios.get(url, {
+        params: {
+          sort: sortOption,
+          audience: audience,
+          category: category,
+        },
+      });
+      setProducts(response.data);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  const contextValue = {
+    products,
+    setProducts,
+    sortOption,
+    setSortOption,
+    category,
+    setCategory,
+    audience,
+    setAudience,
+    fetchProducts,
+  };
+
+  return (
+    <AppContext.Provider value={contextValue}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
