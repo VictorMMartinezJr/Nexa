335d1541e2d324a99c92e49aa6d8e863b942f125
Victor <victorm.martinezjr@outlook.com>
2025-09-24 22:09:47 -0400

    create JwtUtil class

---
+++ backend/nexa/src/main/java/in/victormartinezjr/nexa/util/JwtUtil.java
@@ -0,0 +1,34 @@
+package in.victormartinezjr.nexa.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret.key}")
+    private String SECRET_KEY;
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+
+    }
+
+    private String createToken(Map<String, Object> claims, String email) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + Duration.ofHours(10).toMillis()))
+                .signWith(SignatureAlgorithm.HS256,  SECRET_KEY)
+                .compact();
+    }
+}
