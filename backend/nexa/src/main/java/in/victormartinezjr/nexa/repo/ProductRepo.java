faa3872226a24d5bfb153640afc8da896d174a6e
Victor <victorm.martinezjr@outlook.com>
2025-10-13 03:13:51 -0400

    modify searchProduct method in ProductRepo to search by name, category, and genericCategory

--- backend/nexa/src/main/java/in/victormartinezjr/nexa/repo/ProductRepo.java
+++ backend/nexa/src/main/java/in/victormartinezjr/nexa/repo/ProductRepo.java
@@ -10,6 +10,9 @@ import java.util.List;
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
     @Query("SELECT p from Product p WHERE " +
-    "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+            "LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+    "LOWER(p.genericCategory) LIKE LOWER(CONCAT('%', :keyword, '%')) "
+    )
     List<Product> searchProducts(String keyword);
 }
