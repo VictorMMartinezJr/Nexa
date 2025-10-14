031693648b802a41474b32d8020ffeb693545bd3
Victor <victorm.martinezjr@outlook.com>
2025-10-13 20:22:56 -0400

    create ScrollToTop componenet and add to App.jsx

--- frontend/nexa/src/App.jsx
+++ frontend/nexa/src/App.jsx
@@ -6,17 +6,23 @@ import Products from "./components/pages/Products";
 import Product from "./components/pages/Product";
 import Login from "./components/pages/Login";
 import { ToastContainer } from "react-toastify";
+import Cart from "./components/pages/Cart";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <main id="main" className="bg-white h-screen">
+    <main id="main" className="bg-white min-h-screen">
       <ToastContainer position="top-right" autoClose={3000} />
       <Navbar />
+
+      <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/product/:id" element={<Product />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </main>
   );
