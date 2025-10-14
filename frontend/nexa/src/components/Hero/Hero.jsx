4a196c28ff1896dbc0c27108f2903f45351f864a
Victor <victorm.martinezjr@outlook.com>
2025-10-13 20:51:03 -0400

    add functionality to Hero btn and replace soccer video with basketball

--- frontend/nexa/src/components/Hero/Hero.jsx
+++ frontend/nexa/src/components/Hero/Hero.jsx
@@ -1,5 +1,18 @@
-import soccer from "../../assets/soccer.mp4";
+import { useContext } from "react";
+import hero from "../../assets/hero.mp4";
+import { AppContext } from "../../context/AppContext";
+import { useNavigate } from "react-router-dom";
+
 const Hero = () => {
+  const { setCategory, setAudience } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    setAudience(null);
+    setCategory("Basketball");
+    navigate("/products");
+  };
+
   return (
     <div className="w-full h-[92vh]">
       <div className="relative w-full h-full">
@@ -10,18 +23,21 @@ const Hero = () => {
           muted
           loop
           playsInline
-          aria-label="Video showing a soccer ball being kicked into a goal"
+          aria-label="Video showing a basketball going into a hoop"
         >
-          <source src={soccer} type="video/mp4" />
+          <source src={hero} type="video/mp4" />
         </video>
 
         {/* Overlay text */}
         <div className="flex flex-col justify-center items-center gap-4 absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-3/4 text-white text-center w-full">
           <p className="font-bold text-4xl tracking-tighter md:text-5xl xl:text-9xl">
-            CHASE THE GOAL
+            SHOOT FOR GREATNESS
           </p>
-          <p className="md:text-lg">Shop 2026 World Cup Collection</p>
-          <button className="bg-white text-black px-4 py-1.5 rounded-2xl cursor-pointer lg:px-6">
+          <p className="md:text-xl">Shop Basketball</p>
+          <button
+            className="bg-white text-black px-4 py-1.5 rounded-2xl cursor-pointer lg:px-6"
+            onClick={handleClick}
+          >
             Shop
           </button>
         </div>
